85993e731bda0fdbef8c90e585250abca3d1182c
peter.bambura <peter.bambura@entrepot.sk>
2026-05-11 20:35:21 +0200

    Upravene GUI

--- MojaPrvaAvalonia/Views/UserControls/UcStatusPlc.axaml.cs
+++ MojaPrvaAvalonia/Views/UserControls/UcStatusPlc.axaml.cs
@@ -7,24 +7,8 @@ namespace MojaPrvaAvalonia.Views.UserControls;
 
 public partial class UcStatusPlc : UserControl
 {
-    public static readonly StyledProperty<bool> ShowSetupButtonProperty =
-        AvaloniaProperty.Register<UcStatusPlc, bool>(nameof(ShowSetupButton), defaultValue: false);
-
-    public bool ShowSetupButton
-    {
-        get => GetValue(ShowSetupButtonProperty);
-        set => SetValue(ShowSetupButtonProperty, value);
-    }
-
-    public event EventHandler<RoutedEventArgs>? SetupClicked;
-
     public UcStatusPlc()
     {
         InitializeComponent();
-    }
-
-    private void OnSetupClicked(object? sender, RoutedEventArgs e)
-    {
-        SetupClicked?.Invoke(this, e);
     }
 }
