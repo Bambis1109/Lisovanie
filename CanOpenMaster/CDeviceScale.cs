a556952eb787d50fba8665f5cdc0fed9dc553ec0
Bambis1109 <peter.bambura@entrepot.sk>
2026-06-02 10:41:39 +0200

    test AI

--- CanOpenMaster/CDeviceScale.cs
+++ CanOpenMaster/CDeviceScale.cs
@@ -132,6 +132,7 @@ namespace EposCmd.Net
             }
         }
 
+        
         public void WaitForInitAttained(uint timeoutMs)
         {
             long startTime = Environment.TickCount64;
